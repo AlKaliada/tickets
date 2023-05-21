2ca48b8aeda05eb41fd23de790d1e5eca30345f3
Aliaksei Kaliada <aliaksei.kaliada@gmail.com>
2023-05-21 14:25:49 +0200

    Revert "Initial commit"
    This reverts commit a59c6e9bf0c7ea0a6507e6e7aba0bd551a0a689c.

--- src/main/java/com/kaliada/tickets/repository/TodayActivitiesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kaliada.tickets.repository;
-
-import com.kaliada.tickets.entity.TodayActivities;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodayActivitiesRepository extends JpaRepository<TodayActivities, Long> {
-}
